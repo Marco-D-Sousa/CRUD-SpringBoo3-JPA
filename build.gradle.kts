2de0909d1ab55925281c7d10703270225692eeb3
Marco Paiva <marcopaiva2004@gmail.com>
2024-05-08 13:52:02 +0200

    H2 database and JPA

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,10 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	// https://mvnrepository.com/artifact/org.springframework.boot/spring-boot-starter-data-jpa
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.5")
+	// https://mvnrepository.com/artifact/com.h2database/h2
+	implementation("com.h2database:h2:2.2.224")
 }
 
 tasks.withType<Test> {
